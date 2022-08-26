4cbea0c72644c1d397d684ac356d95b0ec8b721f
Michael Mattie <codermattie@runbox.com>
2022-08-26 11:49:53 -0700

    install shell code to .zshrc including functions to switch between virtual environments

--- pythonsh/python.sh
+++ pythonsh/python.sh
@@ -25,27 +25,80 @@ case $1 in
 #
 # tooling
 #
-    "install-tools")
+    "tools-install")
+        echo "installing brew tools"
+
         brew update
 
         brew install pyenv
         brew install pyenv-virtualenv
         brew install git-flow
+
+        echo "adding shell code to .zshrc, you may need to edit the file."
+
+        cat >>~/.zshrc <<SHELL
+eval "\$(homebrew/bin/brew shellenv)"
+
+export PYENV_ROOT="\$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="\$PYENV_ROOT/bin:\$PATH"
+eval "\$(pyenv init -)"
+
+export EDITOR=$EDITOR
+
+function dev {
+    if test -f python.sh
+    then
+        echo "switching to \${VIRTUAL_PREFIX} dev"
+        source python.sh
+
+        if pyenv virtualenvs | grep '*'
+        then
+            pyenv deactivate
+        fi
+
+        pyenv activate \${VIRTUAL_PREFIX}_dev
+    else
+        echo "cant find python.sh - are you in the project root?"
+    fi;
+}
+
+function release {
+    if test -f python.sh
+    then
+        echo "switching to \${VIRTUAL_PREFIX} release"
+        source python.sh
+
+        if pyenv virtualenvs | grep '*'
+        then
+            pyenv deactivate
+        fi
+
+        pyenv activate \${VIRTUAL_PREFIX}_release
+    else
+        echo "cant find python.sh - are you in the project root?"
+    fi;
+}
+SHELL
     ;;
-    "update-tools")
+    "tools-update")
         brew update
 
         brew upgrade pyenv
         brew upgrade pyenv-virtualenv
         brew upgrade git-flow
-    ;;
 
+        if pyenv virtualenvs | grep '*'
+        then
+           pyenv exec python -m pip install -U pip
+           pyenv exec python -m pip install -U pipenv
+        fi
+    ;;
 #
 # virtual environments
 #
 
     "virtual-install")
-        pyenv install --skip-existing "$PACKAGE_PYTHON_VERSION"
+        pyenv install --skip-existing "$PYTHON_VERSION"
 
         LATEST=$(pyenv versions | grep -E '^ *\d+\.\d+\.\d+$' | sed 's/ *//g')
 
@@ -61,6 +114,14 @@ case $1 in
 
     "virtual-list")
         pyenv virtualenvs
+    ;;
+    "dev")
+        pyenv deactivate
+        pyenv activate ${VIRTUAL_PREFIX}_dev
+    ;;
+    "release")
+        pyenv deactivate
+        pyenv activate ${VIRTUAL_PREFIX}_release
     ;;
 
 #
@@ -195,14 +256,17 @@ python.sh
 
 [tools commands]
 
-install-tools = install tools from homebrew
-update-tools  = update tools from homebrew
+tools-install = install tools from homebrew
+tools-update  = update tools from homebrew
 
 [virtual commands]
 
 virtual-install  = install a pyenv virtual environment
 virtual-destroy  = delete the pyenv virtual environment
 virtual-list     = list virtual environments
+
+dev              = switch to dev environment
+release          = switch to release environment
 
 [python commands]
 
