bfa0948520ccf9d4a3c236116c4886cc0dfc42af
Michael Mattie <codermattie@runbox.com>
2022-08-27 18:12:12 -0700

    make a public repo reference for the public to install and update the docs

--- pythonsh/pysh-install.sh
+++ pythonsh/pysh-install.sh
@@ -10,6 +10,11 @@ case $1 in
 		git submodule add -f git@github.com:coderofmattie/pythonsh.git pythonsh $@
 		test -e py.sh || ln -s pythonsh/pythonsh/python.sh py.sh
 	;;
+	"public")
+		shift
+		git submodule add -f https://github.com/coderofmattie/pythonsh.git pythonsh $@
+		test -e py.sh || ln -s pythonsh/pythonsh/python.sh py.sh
+	;;
 	"remove")
 		git rm pythonsh
 		git rm --cached pythonsh
