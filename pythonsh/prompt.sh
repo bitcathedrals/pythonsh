19339c30b719977a0e1a8fbb80e7572a9061b87a
Michael Mattie <codermattie@runbox.com>
2024-01-27 18:12:22 -0800

    (feat) speed up the prompt massively and clear it when not at the root of the repo while also adding staged and dirty flags

--- pythonsh/prompt.sh
+++ pythonsh/prompt.sh
@@ -7,37 +7,56 @@ git_branch="?"
 project_name="?"
 
 function get_pyenv {
-	environment=`pyenv virtualenvs | grep '*'`
+  environment=`pyenv virtualenvs | grep '*'`
 
-	if [[ -z $environment ]]
-	then
-		virtual_environmeent="none"
-	else
-		virtual_environment=`echo $environment | cut -d ' ' -f 2`
-	fi
+  if [[ -z $environment ]]
+  then
+    virtual_environment="NA"
+  else
+    virtual_environment=`echo $environment | cut -d ' ' -f 2`
+  fi
 }
 
-function get_git {
-	test -d .git || return 1
+function get_branch {
+  git_branch=`git branch | grep '*' | cut -d ' ' -f 2`
 
-	git_branch=`git branch | grep '*' | cut -d ' ' -f 2`
+  if git status | grep -i "changes not staged" >/dev/null
+  then
+    git_flags="*"
+  fi
 
-	return 0
+  if git status | grep -i "changes to be committed" >/dev/null
+  then
+    git_flags="${git_flags}+"
+  fi
+
+  if [[ -n $git_flags ]]
+  then
+    git_branch="${git_branch}(${git_flags})"
+  fi
 }
 
 function get_project {
-	project_name=`basename $PWD`
+  project_name=`basename $PWD`
 }
 
+function is_git {
+  test -d .git && return 0
+
+  return 1
+}
 
 function pythonsh_prompt_update {
-	if get_git
-	then
-		get_pyenv
-		get_project
-	fi
+  if is_git
+  then
+    get_pyenv
+    get_branch
+    get_project
 
-	export PS1="[${virtual_environment}:${git_branch}] (${project_name}) -> "
+    export PS1="[${virtual_environment}] ${project_name}:${git_branch} -> "
+  else
+    export PS1="-> "
+  fi
 }
 
 
