7068604d8a1a4cbd6f9d5098f7686b257f19423f
Michael Mattie <codermattie@runbox.com>
2024-02-19 12:06:39 -0800

    (fix) add tests for expand_version

--- tests/test_python_versions.py
+++ tests/test_python_versions.py
@@ -1,6 +1,14 @@
 import pytest
 
-from pyutils.catpip import get_python_feature, strip_pipfile_version_operators, get_pipfile_version
+from pyutils.catpip import expand_version, get_python_feature, strip_pipfile_version_operators, get_pipfile_version
+
+def test_expand_version_minor():
+    assert "1.2.0" == expand_version("1.2")
+
+    assert "1.0.0" == expand_version("1")
+
+def test_expand_version_pass():
+    assert "1.2.0" == expand_version("1.2")
 
 def get_version_feature():
     assert "1.2" == get_python_feature("1.2.3")
