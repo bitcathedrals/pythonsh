31f96a77ea134da81e667a26828bfc2c071a94fa
Michael Mattie <codermattie@runbox.com>
2024-03-24 14:18:55 -0700

    (feat): add a timestamp label to the docker build [docker-python.org,mkdocker.sh]

--- docker/mkdocker.sh
+++ docker/mkdocker.sh
@@ -5,5 +5,8 @@ template=${script}/Dockerfile.template
 
 DOCKER_VERSION=$1
 PYTHON_VERSION=$2
+TIMESTAMP=$3
 
-sed <$template -e "s,@DOCKER_VERSION@,${DOCKER_VERSION},g" | sed -e "s,@PYTHON_VERSION@,${PYTHON_VERSION},g"
+sed <$template -e "s,@DOCKER_VERSION@,${DOCKER_VERSION},g" |\
+  sed -e "s,@PYTHON_VERSION@,${PYTHON_VERSION},g" |\
+  sed -e "s,@TIMESTAMP@,${TIMESTAMP},g"
