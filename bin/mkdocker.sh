9ca55e081557d6379fc612be5667eb3afc6b2e8c
Michael Mattie <codermattie@runbox.com>
2024-03-27 13:43:45 -0700

    (fix) get the date inside mkdocker.sh since it doesnt need an external value

--- bin/mkdocker.sh
+++ bin/mkdocker.sh
@@ -3,6 +3,6 @@
 template=docker/Dockerfile.template
 
 DOCKER_VERSION=$1
-TIMESTAMP=$2
+TIMESTAMP=`date`
 
 sed <$template -e "s,@DOCKER_VERSION@,${DOCKER_VERSION},g" | sed -e "s,@TIMESTAMP@,${TIMESTAMP},g"
