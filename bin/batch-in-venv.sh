bdf40cc618387a6be8a64e923da1390cdee7af77
Michael Mattie <codermattie@runbox.com>
2024-03-26 17:27:22 -0700

    (feat) add the batch-in-venv.sh command to allow commands to be appended to the file

---
+++ bin/batch-in-venv.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/env bash
+
+VENV="$1"
+shift
+
+if [[ -z $VENV ]]
+then
+  echo >/dev/stderr "run-in-venv.sh: VENV arg(1) not specified. exiting."
+  exit 1
+fi
+
+TOOLS=$HOME/tools
+PYENV_ROOT="$TOOLS/pyenv"
+
+PATH="$TOOLS/local/bin:$PATH"
+PATH="$PYENV_ROOT/bin:$PATH"
+PATH="$PYENV_ROOT/libexec:$PATH"
+
+export PYENV_ROOT PATH
+
+eval "$(pyenv init -)"
+
+pyenv activate $VENV
+
+if [[ $? -ne 0 ]]
+then
+  echo >/dev/stderr "run-in-venv.sh: pyenv activate $VENV failed - $output exiting."
+  exit 1
+fi
+
+### CODE HERE ###
